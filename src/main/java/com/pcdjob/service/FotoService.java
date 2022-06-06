239bae3593f20b5140519b40722bb94192fb0996
Welington Pincer <89099924+pinceru@users.noreply.github.com>
2022-06-06 13:46:32 -0300

    Add files via upload

--- src/main/java/com/pcdjob/service/FotoService.java
+++ src/main/java/com/pcdjob/service/FotoService.java
@@ -1,12 +1,17 @@
 package com.pcdjob.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pcdjob.model.candidato.CandidatoEntity;
 import com.pcdjob.model.candidato.CandidatoFoto;
+import com.pcdjob.model.empresa.EmpresaEntity;
+import com.pcdjob.model.empresa.EmpresaFoto;
 import com.pcdjob.repository.CandidatoFotoRepository;
 import com.pcdjob.repository.EmpresaFotoRepository;
+import com.pcdjob.service.helper.Verificar;
 
 @Service
 public class FotoService {
@@ -18,12 +23,27 @@ public class FotoService {
 
 	public void salvarImagemCandidato(byte[] foto, CandidatoEntity candidato) {
 		candidatoFotoRepository.save(new CandidatoFoto(foto, candidato));
-		
-	}
-
-	public CandidatoFoto buscarFotoCandidatoID(Long id) {
-		return candidatoFotoRepository.findById(id).get();
 	}
 	
+	public void salvarImagemEmpresa(byte[] foto, EmpresaEntity empresa) {
+		empresaFotoRepository.save(new EmpresaFoto(foto, empresa));		
+	}
 
+	public CandidatoFoto buscarFotoCandidatoID(CandidatoEntity candidato) {
+		Optional<CandidatoFoto> optional = candidatoFotoRepository.findByCandidato(candidato);
+		if(Verificar.verificarOptional(optional)) {
+			return optional.get();
+		} else {
+			throw new NullPointerException();
+		}
+	}
+
+	public EmpresaFoto buscarFotoEmrpesaID(EmpresaEntity empresa) {
+		Optional<EmpresaFoto> optional = empresaFotoRepository.findByEmpresa(empresa);
+		if(Verificar.verificarOptional(optional)) {
+			return optional.get();
+		} else {
+			throw new NullPointerException();
+		}
+	}
 }
