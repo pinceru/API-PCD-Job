378892be385f7540f744af5019c0578403124e2a
Welington Pincer <89099924+pinceru@users.noreply.github.com>
2022-06-06 13:47:12 -0300

    Add files via upload

--- src/main/java/com/pcdjob/repository/EmpresaFotoRepository.java
+++ src/main/java/com/pcdjob/repository/EmpresaFotoRepository.java
@@ -1,11 +1,16 @@
 package com.pcdjob.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pcdjob.model.empresa.EmpresaEntity;
 import com.pcdjob.model.empresa.EmpresaFoto;
 
 @Repository
 public interface EmpresaFotoRepository extends JpaRepository<EmpresaFoto, Long> {
+
+	Optional<EmpresaFoto> findByEmpresa(EmpresaEntity empresa);
 
 }
