a828b1fc4c040b248e2f6cc512ef5e2ef9f47d22
Welington Pincer <89099924+pinceru@users.noreply.github.com>
2022-06-06 13:45:15 -0300

    Add files via upload

--- src/main/java/com/pcdjob/controller/ImagemController.java
+++ src/main/java/com/pcdjob/controller/ImagemController.java
@@ -17,7 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 import com.pcdjob.controller.dto.ImagemDTO;
 import com.pcdjob.model.candidato.CandidatoEntity;
 import com.pcdjob.model.candidato.CandidatoFoto;
+import com.pcdjob.model.empresa.EmpresaEntity;
+import com.pcdjob.model.empresa.EmpresaFoto;
 import com.pcdjob.service.CandidatoService;
+import com.pcdjob.service.EmpresaService;
 import com.pcdjob.service.FotoService;
 
 @RestController
@@ -30,14 +33,16 @@ public class ImagemController {
 	@Autowired
 	private CandidatoService candidatoService;
 	
+	@Autowired
+	private EmpresaService empresaService;
 	
 	@CrossOrigin
 	@PostMapping(path = "/candidato/{id}", produces = "application/json")
-	public ResponseEntity<?> postImage(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
+	public ResponseEntity<?> postImageCandidato(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
 		try {
 			if(!file.isEmpty()) {
 				CandidatoEntity candidato = candidatoService.buscarCandidatoID(id);
-				byte[] bytes = file.getBytes();
+				byte[] bytes = Base64Utils.encode(file.getBytes());
 				fotoService.salvarImagemCandidato(bytes, candidato);
 				return ResponseEntity.ok().build();
 			}
@@ -49,11 +54,40 @@ public class ImagemController {
 	
 	@CrossOrigin
 	@GetMapping(path = "/candidato/{id}", produces = "application/json")
-	public ResponseEntity<?> buscarImagem(@PathVariable Long id) {
+	public ResponseEntity<?> buscarImagemCandidato(@PathVariable Long id) {
 		try {
-			CandidatoFoto foto = fotoService.buscarFotoCandidatoID(id);
-			byte[] encoded = Base64Utils.encode(foto.getFoto());
-			return ResponseEntity.ok().body(new ImagemDTO(encoded));
+			CandidatoEntity candidato = candidatoService.buscarCandidatoID(id);
+			CandidatoFoto foto = fotoService.buscarFotoCandidatoID(candidato);
+			return ResponseEntity.ok().body(new ImagemDTO(foto.getFoto()));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@CrossOrigin
+	@PostMapping(path = "/emrpesa/{id}", produces = "application/json")
+	public ResponseEntity<?> postImageEmpresa(@RequestParam("file") MultipartFile file, @PathVariable Long id) {
+		try {
+			if(!file.isEmpty()) {
+				EmpresaEntity empresa = empresaService.buscarEmpresaID(id);
+				byte[] bytes = Base64Utils.encode(file.getBytes());
+				fotoService.salvarImagemEmpresa(bytes, empresa);
+				return ResponseEntity.ok().build();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@CrossOrigin
+	@GetMapping(path = "/empresa/{id}", produces = "application/json")
+	public ResponseEntity<?> buscarImagemEmpresa(@PathVariable Long id) {
+		try {
+			EmpresaEntity empresa = empresaService.buscarEmpresaID(id);
+			EmpresaFoto foto = fotoService.buscarFotoEmrpesaID(empresa);
+			return ResponseEntity.ok().body(new ImagemDTO(foto.getFoto()));
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
